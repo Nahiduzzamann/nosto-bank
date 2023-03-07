0fe2f1935d3c3892aa838fa31d2372041aff0df1
Nahiduzzamann <93345313+Nahiduzzamann@users.noreply.github.com>
2023-03-07 08:01:02 +0000

    ok

--- js/deposit.js
+++ js/deposit.js
@@ -10,7 +10,7 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
     1. get previous deposit total by id
 
     */
-  const previousDepositTotal = getTextElementValueById("deposit-total");
+  const previousDepositTotal = document.getElementById("deposit-total").value;
 
   // calculate new deposit total
   const newDepositTotal = previousDepositTotal + newDepositAmount;
@@ -19,7 +19,7 @@ document.getElementById("btn-deposit").addEventListener("click", function () {
   setTextElementValueById("deposit-total", newDepositTotal);
 
   // get previous balance by using the function
-  const previousBalanceTotal = getextElementValueById("balance-total");
+  const previousBalanceTotal = getElementValueById("balance-total");
   const newBalanceTotal = previousBalanceTotal + newDepositAmount;
   setTextElementValueById("balance-total", newBalanceTotal);
-);
+});
